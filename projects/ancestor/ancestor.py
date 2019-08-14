97a43ff9142920864a7c5eeb65409b857c72150a
Chubbard022 <chubbard022@gmail.com>
2019-08-14 12:05:33 -0700

    checking if child is in graph. implimentation not complete

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,21 @@
+from util import Queue
+
+
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    q = Queue()
+    visited = set()
+    count = 0
+    
+    q.enqueue([starting_node])
+
+    # will return false if the size is less than 1
+    while q.size():
+        child = q.dequeue
+        visited.add(child)
+        for edge in _____:
+            if edge not in visited:
+                q.enqueue(edge)
+                count +=1
+    return count
+
